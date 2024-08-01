a79b91b318a855498c8b95ffa982c3948154a307
WYJLMT <wyj11281212@163.com>
2024-07-31 20:29:12 -0700

    test of multi-tests, finished!

---
+++ tests/test01-Pi/performance/exec.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 脚本文件路径
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+FUNC_SCRIPT_DIR="../function"
+EXEC_CMD="../../performance_counter_920.sh ${FUNC_SCRIPT_DIR}/exec.sh ./res.txt"
+
+# 执行EXEC_CMD命令
+${EXEC_CMD}
+
+# 无论EXEC_CMD的执行结果如何，直接写入result.json文件
+cat <<EOF >"${SCRIPT_DIR}/result.json"
+{
+    "name": "performance",
+    "res": "ok"
+}
+EOF
+
+# @ 2024-07-31, this is used to test the testing-system, just write res, now
